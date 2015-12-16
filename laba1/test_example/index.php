3e1bf24f44c574fffa102dcfc1d7fc8db8d5be2c
Maxghp <allmydocin@gmail.com>
2015-12-16 21:04:03 +0200

    Update index.php

--- laba1/test_example/index.php
+++ laba1/test_example/index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Denis
- * Date: 14.09.2015
- * Time: 1:15
- */
 header("Content-type: text/html; charset=utf-8");
 if (!isset($_POST['test'])) {
     $title = 'Пройдите тест!';
